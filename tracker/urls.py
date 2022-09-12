09f2e23b2a620044142f410fb51e7c463602d588
James Hoffman <jamesvhoffman@outlook.com>
2022-09-12 17:33:45 -0400

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
     path(
         "",
         RedirectView.as_view(url=reverse_lazy("list_projects")),
